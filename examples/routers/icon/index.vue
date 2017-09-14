ebc047e37987a225ba00b26d0bc85fc9ccea5c34
Yaozong Sun <zzuu666@outlook.com>
2017-09-14 20:24:30 +0800

    Finished the Icon component

---
+++ examples/routers/icon/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ant-icon type="link" />
+  </div>
+</template>
+
+<script>
+import AntIcon from '@/icon'
+export default {
+  components: {
+    AntIcon
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
