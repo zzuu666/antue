28651a3c38036e8fa0fe98660004ed5a614cab94
Yaozong Sun <zzuu666@outlook.com>
2017-09-21 18:59:44 +0800

    remove some old generate file

--- build/to.js
+++
@@ -1,7 +0,0 @@
-/**
- * For async/await function catch error without try/catch blocks
- * @param {*} promise 
- */
-const to = promise => promise.then(data => [null, data]).catch(err => [err])
-
-module.exports = to
