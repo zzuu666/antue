892772086ceea55735be3645f0e6bc3a0a4cf4fd
sky-ADMIN <546485299@qq.com>
2017-09-25 17:07:46 +0800

    add input addon example

---
+++ examples/routers/input/demo/addon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ant-input-addon">
+    <ant-input v-model="inputData" placeholder="Enter your username" addonBefore="Http://" addonAfter=".com">
+      <!--<ant-icon slot="prefix" type="user"></ant-icon>-->
+    </ant-input>
+  </div>
+</template>
+
+<script>
+  import AntInput from '@/input'
+  import AntIcon from '@/icon'
+
+  export default {
+    data () {
+      return {
+        inputData: ''
+      }
+    },
+    components: {
+      AntInput,
+      AntIcon
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .ant-input-addon {
+    padding: 10px;
+  }
+</style>
--- examples/routers/input/index.vue
+++ examples/routers/input/index.vue
@@ -9,16 +9,17 @@
         <code-show title="基本使用" desc="基本使用">
           <base-demo></base-demo>
         </code-show>
+        <code-show title="前置/后置标签" desc="用于配置一些固定组合。">
+          <addon-demo></addon-demo>
+        </code-show>
+        <code-show title="前缀和后缀" desc="在输入框上添加前缀或后缀图标。">
+          <prefix-demo></prefix-demo>
+        </code-show>
       </a-col>
       <a-col :span="12">
         <code-show title="三种大小" desc="我们为 <input /> 输入框定义了三种尺寸（大、默认、小），高度分别为 32px、28px 和 22px。
 注意： 在表单里面，我们只使用大尺寸的输入框。">
           <size-demo></size-demo>
-        </code-show>
-      </a-col>
-      <a-col :span="12">
-        <code-show title="前缀和后缀" desc="在输入框上添加前缀或后缀图标。">
-          <prefix-demo></prefix-demo>
         </code-show>
       </a-col>
     </a-row>
@@ -33,6 +34,7 @@
   import BaseDemo from './demo/base'
   import SizeDemo from './demo/size'
   import PrefixDemo from './demo/prefix.vue'
+  import AddonDemo from './demo/addon.vue'
 
   export default {
     components: {
@@ -42,11 +44,14 @@
       BaseDemo,
       CodeShow,
       SizeDemo,
-      PrefixDemo
+      PrefixDemo,
+      AddonDemo
     }
   }
 </script>
 
 <style lang="less" scoped>
-
+.code-show {
+  margin-top: 20px;
+}
 </style>
