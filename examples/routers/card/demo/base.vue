faacd324f9a0844a882083e687367d3c133fc752
echaoo <zmk181@gmail.com>
2017-09-26 22:45:09 +0800

    add base card

---
+++ examples/routers/card/demo/base.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ant-card-base">
+    <ant-card title="Card title" extra="more" style="width: 300px">
+      <a href="#" slot="extra">More</a>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </ant-card>
+  </div>
+</template>
+
+<script>
+  import AntCard from '@/card'
+  export default {
+    components: {
+      AntCard
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .ant-card-base {
+    padding: 10px 30px;
+    text-align: left;
+  }
+</style>
