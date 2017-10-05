6e443f5642f21601050d32e31cd098e714d7f548
echaoo <1299529170@qq.com>
2017-10-05 11:17:27 +0800

    add card grid component

---
+++ components/card/grid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="[`${prefixCls}-grid`, className]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      prefixCls: {
+        type: String,
+        default: 'ant-card'
+      },
+      className: String
+    }
+  }
+</script>
--- components/card/index.js
+++ components/card/index.js
@@ -4,5 +4,7 @@
 import './style/index.js'
 
 import Card from './card'
+import Grid from './grid'
 
+Card.grid = Grid
 export default Card
