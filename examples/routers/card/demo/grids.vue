2175d90779b5f7fe406485104f22839989a6414f
echaoo <1299529170@qq.com>
2017-10-07 12:37:27 +0800

    update cart component and demo

--- examples/routers/card/demo/grids.vue
+++ examples/routers/card/demo/grids.vue
@@ -1,21 +1,19 @@
 <template>
-  <div :class="[
-    'ant-card-base',
-    !bordered ? `ant-bgc` : '']">
+  <div class="ant-card-base ant-bgc">
     <a-row :gutter="12">
       <a-col :span="8">
-        <ant-card :title="title" :extra="extra" :bordered="bordered">
-          <p>Card content</p>
+        <ant-card title="Card Title" :bordered="false">
+          Card content
         </ant-card>
       </a-col>
       <a-col :span="8">
-        <ant-card :title="title" :extra="extra" :bordered="bordered">
-          <p>Card content</p>
+        <ant-card title="Card Title" :bordered="false">
+          Card content
         </ant-card>
       </a-col>
       <a-col :span="8">
-        <ant-card :title="title" :extra="extra" :bordered="bordered">
-          <p>Card content</p>
+        <ant-card title="Card Title" :bordered="false">
+          Card content
         </ant-card>
       </a-col>
     </a-row>
@@ -31,14 +29,6 @@
       AntCard,
       ARow,
       ACol
-    },
-    props: {
-      bordered: {
-        type: Boolean,
-        default: true
-      },
-      title: String,
-      extra: String
     }
   }
 </script>
