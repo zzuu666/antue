8d975d937c504da556fc6efecf29ad0a59684b59
echaoo <1299529160@qq.com>
2017-10-15 00:34:00 +0800

    update radio-button

--- components/radio/index.js
+++ components/radio/index.js
@@ -5,6 +5,8 @@ import './style/index.js'
 
 import Radio from './radio.vue'
 import Group from './group.vue'
+import RadioButton from './radio-button'
 
 Radio.group = Group
+Radio.radioButton = RadioButton
 export default Radio
