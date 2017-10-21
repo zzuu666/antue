3c4304fe86666e890ffc99d5d2cb0e1dad564a0b
sky-ADMIN <546485299@qq.com>
2017-10-21 23:08:01 +0800

    add pagination component

---
+++ components/pagination/Pagination.vue
@@ -0,0 +1,200 @@
+<template>
+  <ul :class="`${this.prefixCls}`">
+    <li
+      tabIndex="0"
+      :class="`${hasPrev ? '' : `${prefixCls}-disabled`} ${prefixCls}-prev`"
+      :title="showTitle ? locale.prev_page : null"
+      :aria-disabled="!hasPrev"
+      @click="prev"
+      @keypress="runIfEnterPrev"
+    >
+      <a class="ant-pagination-item-link"></a>
+    </li>
+
+    <!--first page : when current -->
+    <li tabIndex="0" @click="handleChange(1)" :class="`${this.prefixCls}-item`" v-if="pageRange[0] !== 1">
+      <a>1</a>
+    </li>
+
+    <li
+      tabIndex="0"
+      v-if="showJumpPrev"
+      :class="`${prefixCls}-jump-prev`"
+      @click="jumpPrev"
+      @keypress="runIfEnterJumpPrev"
+    >
+      <a :class="`${prefixCls}-item-link`"></a>
+    </li>
+
+    <li tabIndex="0" v-for="i in pageRange" :class="pagerClasses(i)" @click="clickPage(i)" ref="page">
+      <a>{{i}}</a>
+    </li>
+
+    <li
+      v-if="showJumpNext"
+      tabIndex="0"
+      @click="jumpNext"
+      @keypress="runIfEnterJumpNext"
+      :class="`${prefixCls}-jump-next`"
+    >
+      <a :class="`${prefixCls}-item-link`"></a>
+    </li>
+
+    <!--last page : when current < last - 5-->
+    <li tabIndex="0" @click="handleChange(calculatePage)" :class="`${this.prefixCls}-item`" v-if="pageRange[pageRange.length-1] !== calculatePage">
+      <a>{{calculatePage}}</a>
+    </li>
+
+    <li
+      tabIndex="0"
+      :aria-disabled="!hasNext"
+      :class="`${this.hasNext ? '' : `${prefixCls}-disabled`} ${prefixCls}-next`"
+      @click="next"
+      @keydown="runIfEnterNext"
+    >
+      <a :class="`${prefixCls}-item-link`"></a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  //  import KEYCODE from './KeyCode'
+  import LOCALE from './locale/zh_CN'
+
+  export default {
+    props: {
+      current: {
+        type: Number,
+        default: 1
+      },
+      pageSize: {
+        type: Number,
+        default: 10
+      },
+      prefixCls: {
+        type: String,
+        default: 'ant-pagination'
+      },
+      showQuickJumper: {
+        type: Boolean,
+        default: false
+      },
+      showSizeChanger: {
+        type: Boolean,
+        default: false
+      },
+      showTitle: Boolean,
+      showTotal: Function,
+      simple: Boolean,
+      size: String,
+      total: {
+        type: Number,
+        default: 0
+      }
+    },
+    data () {
+      return {
+        locale: LOCALE,
+        currentPage: this.current,
+        changeByPager: false
+      }
+    },
+    computed: {
+      calculatePage () {
+        return Math.floor((this.total - 1) / this.pageSize) + 1
+      },
+      hasPrev () {
+        return this.currentPage > 1
+      },
+      hasNext () {
+        return this.currentPage < this.calculatePage
+      },
+      pageRange () {
+        let rs = []
+        if (this.calculatePage > 5) {
+          if (this.currentPage > 2 && this.currentPage < this.calculatePage - 2) {
+            rs = this.range(this.currentPage - 2, this.currentPage + 2)
+          } else if (this.currentPage <= 2) {
+            rs = this.range(1, 5)
+          } else {
+            rs = this.range(this.calculatePage - 5, this.calculatePage)
+          }
+        } else {
+          rs = this.range(1, this.calculatePage)
+        }
+        return rs
+      },
+      showJumpNext () {
+        return this.calculatePage - this.currentPage > 3
+      },
+      showJumpPrev () {
+        return this.currentPage > 4
+      }
+    },
+    watch: {
+      current () {
+        this.currentPage = this.current
+      },
+      pageRange () {
+        if (this.pageRange[0] !== 1 && this.pageRange[this.pageRange.length - 1] !== this.calculatePage && this.changeByPager) {
+          this.changeByPager = false
+          this.$refs.page[2].focus()
+        }
+      }
+    },
+    methods: {
+      clickPage (i) {
+        this.changeByPager = true
+        this.handleChange(i)
+      },
+      handleChange (i) {
+        this.currentPage = i
+        this.$emit('change-page', i)
+      },
+      handleGoTO () {},
+      jumpNext () {
+        this.calculatePage - this.currentPage > 5 ? this.handleChange(this.currentPage + 5) : this.handleChange(this.calculatePage)
+      },
+      jumpPrev () {
+        this.currentPage > 5 ? this.handleChange(this.currentPage - 5) : this.handleChange(1)
+      },
+      next () {
+        if (this.hasNext) {
+          this.handleChange(this.currentPage + 1)
+        }
+      },
+      pagerClasses (i) {
+        return [
+          `${this.prefixCls}-item`,
+          {
+            [`${this.prefixCls}-item-active`]: this.currentPage === i
+          }
+        ]
+      },
+      prev () {
+        if (this.hasPrev) {
+          this.handleChange(this.currentPage - 1)
+        }
+      },
+      range (start, end) {
+        let arr = []
+        for (let i = start; i <= end; i++) {
+          arr.push(i)
+        }
+        return arr
+      },
+      runIfEnterJumpNext (event) {
+        (event.key === 'Enter' || event.charCode === 13) && this.jumpNext()
+      },
+      runIfEnterJumpPrev (event) {
+        (event.key === 'Enter' || event.charCode === 13) && this.jumpPrev()
+      },
+      runIfEnterNext (event) {
+        (event.key === 'Enter' || event.charCode === 13) && this.next()
+      },
+      runIfEnterPrev (event) {
+        (event.key === 'Enter' || event.charCode === 13) && this.prev()
+      }
+    }
+  }
+</script>
---
+++ components/pagination/index.js
@@ -0,0 +1,5 @@
+import './style/index.js'
+import Pagination from './Pagination'
+
+export { PaginationProps } from './Pagination'
+export default Pagination
