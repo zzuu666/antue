889ee5c0d4f77c111d804b6b9d73ad82310b5cc8
Huanyu Li <546485299@qq.com>
2017-10-27 17:52:16 +0800

    add input group component

--- components/input/index.js
+++ components/input/index.js
@@ -2,8 +2,9 @@ import './style/index.js'
 import Input from './input'
 import Search from './search'
 import TextArea from './textarea'
+import Group from './group'
 
-// Input.Group = Group
+Input.Group = Group
 Input.Search = Search
 Input.TextArea = TextArea
 export default Input
