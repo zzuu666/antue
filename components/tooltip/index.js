44039f9095b51e94d8459d4591bf6dbe7d4dd15d
Yaozong Sun <zzuu666@outlook.com>
2017-11-07 20:54:39 +0800

    add tooltip component

---
+++ components/tooltip/index.js
@@ -0,0 +1,4 @@
+import './style/index.js'
+import Tooltip from './tooltip.js'
+
+export default Tooltip
---
+++ components/tooltip/tooltip.js
@@ -0,0 +1,27 @@
+import TooltipMixin from '../_mixin/tooltip'
+
+export default {
+  name: 'tooltip',
+  mixins: [TooltipMixin],
+  props: {
+    prefixCls: {
+      type: String,
+      default: 'ant-tooltip'
+    }
+  },
+  components: {
+    /**
+     * 定义函数化组件用于插入到popperVM
+     * 实际浮层显示的内容
+     */
+    AtuTooltipContent: {
+      functional: true,
+
+      render (h, context) {
+        return h('div', {
+          'class': ['ant-tooltip-inner']
+        }, context.data.title)
+      }
+    }
+  }
+}
