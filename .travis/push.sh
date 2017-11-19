f865f3f0bffd3ea074182caebeeb5f89f5b5a3e6
Huanyu Li <546485299@qq.com>
2017-11-19 18:44:45 +0800

    update agile config to make docs update automatic

---
+++ .travis/push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "546485299@qq.com"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  cp ./site/dist/* ./
+  git add .
+  git commit --message "Travis update: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add gh-pages https://${GH_TOKEN}@github.com/zzuu666/antue.git
+  git push --quiet
+}
+
+setup_git
+commit_website_files
+upload_files
