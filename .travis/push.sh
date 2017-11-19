59137a51205a58f3ac1b5a537a2e0c5c307b3a67
Huanyu Li <546485299@qq.com>
2017-11-19 21:05:26 +0800

    try again

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,18 +3,19 @@
 setup_git() {
   git config --global user.email "546485299@qq.com"
   git config --global user.name "Travis CI"
+  git remote add origin https://${GH_TOKEN}@github.com/zzuu666/antue.git
+  git pull
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout gh-pages
   cp ./site/dist/* ./
   git add .
   git commit -m "Travis update: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin/gh-pages https://${GH_TOKEN}@github.com/zzuu666/antue.git
-  git push --quiet --set-upstream origin gh-pages
+  git push --quiet
 }
 
 setup_git
