d30c2d399e8c8197197c0a8a1fdfa96aa68b5654
loocao <dev@loocao.vip>
2017-12-09 17:01:29 +0800

    TODO checkbox

---
+++ components/checkbox/checkbox.vue
@@ -0,0 +1,52 @@
+<template>
+  <label :class="prefixCls + '-wrapper'">
+    <span :class="classes"><input type="checkbox" :class="prefixCls + '-input'"><span
+      :class="prefixCls + '-inner'"/></span>
+    <span v-if="$slots.default !== undefined"><slot></slot></span>
+  </label>
+</template>
+
+<script>
+  export default {
+    name: 'checkbox',
+    props: {
+      autoFocus: {
+        type: Boolean,
+        default: false
+      },
+      checked: {
+        type: Boolean,
+        default: false
+      },
+      defaultChecked: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      indeterminate: {
+        type: Boolean,
+        default: false
+      },
+      prefixCls: {
+        type: String,
+        default: 'ant-checkbox'
+      }
+    },
+    computed: {
+      classes () {
+        const prefixCls = this.prefixCls
+        return [
+          `${prefixCls}`,
+          {
+            [`${prefixCls}-checked`]: !!this.checked,
+            [`${prefixCls}-disabled`]: !!this.disabled,
+            [`${prefixCls}-indeterminate`]: !!this.indeterminate
+          }
+        ]
+      }
+    }
+  }
+</script>
---
+++ examples/routers/checkbox/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <container
+    title="Checkbox 多选框"
+    profile="多选框。">
+    <h2>何时使用</h2>
+    <p>在一组可选项中进行多项选择时；</p>
+    <p>单独使用可以表示两种状态之间的切换，和 switch 类似。区别在于切换 switch 会直接触发状态改变，而 checkbox 一般用于状态标记，需要和提交操作配合。</p>
+    <h2>代码演示</h2>
+
+    <a-row :gutter="12">
+      <a-col :span="12">
+        <code-show title="基本用法" desc="简单的 checkbox。">
+          <demo-base></demo-base>
+        </code-show>
+      </a-col>
+    </a-row>
+  </container>
+</template>
+
+<script>
+  import Container from '../../common/container'
+  import CodeShow from '../../common/code-show'
+  import ARow from '@/row'
+  import ACol from '@/col'
+  import DemoBase from './demo/base'
+
+  export default {
+    components: {
+      Container,
+      CodeShow,
+      ARow,
+      ACol,
+      DemoBase
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
