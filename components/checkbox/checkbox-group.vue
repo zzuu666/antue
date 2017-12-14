dab97462160de6ad36d327af33528a06d30cca0e
loocao <dev@loocao.vip>
2017-12-14 18:14:44 +0800

    checkbox-group

---
+++ components/checkbox/checkbox-group.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="prefixCls">
+    <checkbox v-for="(item,index) in items" :key="index"
+              v-model="item.checked" :disabled="disable(item)"
+              :class="prefixCls+'-item'" @change="change">{{ item.label }}
+    </checkbox>
+  </div>
+</template>
+
+<script>
+  import Checkbox from './checkbox'
+
+  export default {
+    name: 'checkbox-group',
+    components: { Checkbox },
+    props: {
+      options: { type: Array, default: [] },
+      prefixCls: { type: String, default: 'ant-checkbox-group' },
+      value: { type: Array, default: [] },
+      disabled: { type: Boolean, default: false }
+    },
+    data () {
+      return {
+        items: this.options.map(item => {
+          const _item = item.constructor === String ? { label: item, value: item } : item
+          return Object.assign({}, _item, { checked: this.value.indexOf(_item.value) > -1 })
+        })
+      }
+    },
+    methods: {
+      change () {
+        const value = this.items.filter(item => item.checked === true).map(item => item.value)
+        this.$emit('input', value)
+        this.$emit('change', value, this.items)
+      },
+      disable (item) {
+        return this.disabled || item.disabled === true
+      }
+    }
+  }
+</script>
---
+++ examples/routers/checkbox/demo/group.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <checkbox-group v-model="value1" :options="options1">Checkbox</checkbox-group>
+    <br/>
+    <checkbox-group v-model="value2" :options="options2">Checkbox</checkbox-group>
+    <br/>
+    <checkbox-group v-model="value2" :options="options2" disabled>Checkbox</checkbox-group>
+  </div>
+</template>
+
+<script>
+  import { CheckboxGroup } from '@/checkbox'
+
+  export default {
+    components: {
+      CheckboxGroup
+    },
+    data: () => ({
+      options1: ['Apple', 'Pear', 'Orange'],
+      value1: ['Apple'],
+      options2: [
+        { label: 'Apple', value: '1' },
+        { label: 'Pear', value: '2' },
+        { label: 'Orange', value: '3' }
+      ],
+      value2: ['2']
+    }),
+    watch: {
+      value (val) {
+        console.log(val)
+      }
+    }
+  }
+</script>
