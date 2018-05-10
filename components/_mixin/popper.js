0cab6683787abe4af28a765fde70ccc882e98f90
Yaozong Sun <zzuu666@outlook.com>
2018-05-10 12:59:49 +0800

    删除冗余信息

--- components/_mixin/popper.js
+++ components/_mixin/popper.js
@@ -32,19 +32,6 @@ const popperMap = {
   'right-end': 'rightBottom'
 }
 
-const _extends = Object.assign || function (target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i]
-
-    for (var key in source) {
-      if (Object.prototype.hasOwnProperty.call(source, key)) {
-        target[key] = source[key]
-      }
-    }
-  }
-  return target
-}
-
 export default {
   props: {
     placement: {
@@ -98,7 +85,7 @@ export default {
           gpuAcceleration: false
         }
       }
-      options.modifiers = _extends({}, defaultModifiers, this.popperModifiers)
+      options.modifiers = Object.assign({}, defaultModifiers, this.popperModifiers)
       options.onCreate = () => {
         this.currentPalcement = this.popper.getAttribute('x-placement')
         this.resetTransformOrigin()
